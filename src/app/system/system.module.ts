853355abcdb10bc01f21713f7628cb66a5f8edc9
svlugovoy <svlugovoy@gmail.com>
2018-06-14 00:01:38 +0300

    added custom date pipe

--- src/app/system/system.module.ts
+++ src/app/system/system.module.ts
@@ -4,22 +4,25 @@ import {NgModule} from '@angular/core';
 import {SharedModule} from '../shared/shared.module';
 import {SystemRoutingModule} from './system-routing.module';
 import {SystemComponent} from './system.component';
-import { BillPageComponent } from './bill-page/bill-page.component';
-import { HistoryPageComponent } from './history-page/history-page.component';
-import { PlanningPageComponent } from './planning-page/planning-page.component';
-import { RecordsPageComponent } from './records-page/records-page.component';
-import { SidebarComponent } from './shared/components/sidebar/sidebar.component';
-import { HeaderComponent } from './shared/components/header/header.component';
+import {BillPageComponent} from './bill-page/bill-page.component';
+import {HistoryPageComponent} from './history-page/history-page.component';
+import {PlanningPageComponent} from './planning-page/planning-page.component';
+import {RecordsPageComponent} from './records-page/records-page.component';
+import {SidebarComponent} from './shared/components/sidebar/sidebar.component';
+import {HeaderComponent} from './shared/components/header/header.component';
 import {DropdownDirective} from './shared/directives/dropdown.directive';
-import { BillCardComponent } from './bill-page/bill-card/bill-card.component';
-import { CurrencyCardComponent } from './bill-page/currency-card/currency-card.component';
+import {BillCardComponent} from './bill-page/bill-card/bill-card.component';
+import {CurrencyCardComponent} from './bill-page/currency-card/currency-card.component';
 import {BillService} from './shared/services/bill.service';
+import {MomentPipe} from './shared/pipes/moment.pipe';
 
 
 @NgModule({
   declarations: [SystemComponent, BillPageComponent, HistoryPageComponent, PlanningPageComponent,
-   RecordsPageComponent, SidebarComponent, HeaderComponent, DropdownDirective, BillCardComponent, CurrencyCardComponent],
+    RecordsPageComponent, SidebarComponent, HeaderComponent, DropdownDirective, BillCardComponent, CurrencyCardComponent,
+    MomentPipe],
   imports: [CommonModule, SharedModule, SystemRoutingModule],
   providers: [BillService]
 })
-export class SystemModule {}
+export class SystemModule {
+}
