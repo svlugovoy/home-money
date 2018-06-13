fcc9e3ec3531f6817fd4b04b2203575720e6aea5
svlugovoy <svlugovoy@gmail.com>
2018-06-13 16:49:26 +0300

    added async existing email validator for registration

--- src/app/auth/registration/registration.component.ts
+++ src/app/auth/registration/registration.component.ts
@@ -22,7 +22,7 @@ export class RegistrationComponent implements OnInit {
   ngOnInit() {
     this.message = new Message('danger', '');
     this.form = new FormGroup({
-      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'email': new FormControl(null, [Validators.required, Validators.email], this.forbiddenEmails.bind(this)),
       'password': new FormControl(null, [Validators.required, Validators.minLength(6)]),
       'name': new FormControl(null, [Validators.required]),
       'agree': new FormControl(false, [Validators.requiredTrue])
@@ -42,6 +42,19 @@ export class RegistrationComponent implements OnInit {
           }
         });
       });
+  }
+
+  forbiddenEmails(control: FormControl): Promise<any> {
+    return new Promise((resolve, reject) => {
+      this.usersService.getUserByEmail(control.value)
+        .subscribe((user: User) => {
+          if (user) {
+            resolve({forbiddenEmail: true});
+          } else {
+            resolve(null);
+          }
+        });
+    });
   }
 
 }
