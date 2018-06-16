47ddb17c1000343aaaac274df0c3c9529619cc94
svlugovoy <svlugovoy@gmail.com>
2018-06-16 12:41:35 +0300

    implemented lazy loading system module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {NgxChartsModule} from '@swimlane/ngx-charts';
 
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import {AuthModule} from './auth/auth.module';
 import {AppRoutingModule} from './app-routing.module';
 import {HttpModule} from '@angular/http';
 import {UsersService} from './shared/services/users.service';
 import {AuthService} from './shared/services/auth.service';
-import {SystemModule} from './system/system.module';
 import {BaseApi} from './shared/core/base-api';
 
 
@@ -20,7 +18,7 @@ import {BaseApi} from './shared/core/base-api';
   ],
   imports: [
     BrowserModule, HttpModule, BrowserAnimationsModule,
-    AuthModule, AppRoutingModule, SystemModule
+    AuthModule, AppRoutingModule
   ],
   providers: [UsersService, AuthService, BaseApi],
   bootstrap: [AppComponent]
